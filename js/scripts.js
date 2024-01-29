6c0e5ab36dfa4c37033d3e24055099ac4ff5027d
k <k@example.com>
2024-01-29 12:57:06 -0800

    fix map, cleanup, first logo

--- js/scripts.js
+++ js/scripts.js
@@ -159,23 +159,23 @@ $(document).ready(function () {
         },
         data: {
             // Event title
-            title: "Ram and Antara's Wedding",
+            title: "Tracey and Stephen's Wedding",
 
             // Event start date
-            start: new Date('Nov 27, 2017 10:00'),
+            start: new Date('Oct 27, 2024 11:00'),
 
             // Event duration (IN MINUTES)
             // duration: 120,
 
             // You can also choose to set an end time
             // If an end time is set, this will take precedence over duration
-            end: new Date('Nov 29, 2017 00:00'),
+            end: new Date('Oct 27, 2024 9:00'),
 
             // Event Address
-            address: 'ITC Fortune Park Hotel, Kolkata',
+            address: '',
 
             // Event Description
-            description: "We can't wait to see you on our big day. For any queries or issues, please contact Mr. Amit Roy at +91 9876543210."
+            description: ""
         }
     });
 
@@ -215,29 +215,22 @@ $(document).ready(function () {
 
 // Google map
 function initMap() {
-    var location = {lat: 22.5932759, lng: 88.27027720000001};
+    var ceremonyLocation = {lat: 40.05277676277598, lng: -75.32337245210972};
     var map = new google.maps.Map(document.getElementById('map-canvas'), {
         zoom: 15,
-        center: location,
+        center: ceremonyLocation,
         scrollwheel: false
     });
 
     var marker = new google.maps.Marker({
-        position: location,
+        position: ceremonyLocation,
         map: map
     });
-}
 
-function initBBSRMap() {
-    var la_fiesta = {lat: 20.305826, lng: 85.85480189999998};
-    var map = new google.maps.Map(document.getElementById('map-canvas'), {
-        zoom: 15,
-        center: la_fiesta,
-        scrollwheel: false
-    });
+    var banquetLocation = {lat: 40.022860572142974, lng: -75.32603667570083};
 
     var marker = new google.maps.Marker({
-        position: la_fiesta,
+        position: banquetLocation,
         map: map
     });
 }
